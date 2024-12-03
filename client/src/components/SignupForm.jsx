cbf04bcc92f4697c0d2f0839abbbf3db9245f081
yinwu173 <yin.wu173@gmail.com>
2024-12-02 23:09:04 -0800

    edited sign up and login form jsx: replaced adduser functionality and login user functionality from the API file with the Add_User and Login_User mutation functionality.

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations'
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
