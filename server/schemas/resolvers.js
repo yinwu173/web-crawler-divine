b01d8ec750665b423959239a58fbb3f739530134
yinwu173 <yin.wu173@gmail.com>
2024-12-02 22:14:04 -0800

    edited resolvers.js: defined the mutation functionality to work with the Mongoose models.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,22 +5,69 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
     Query: {
         me: async (parent, args, context) => {
-          if (context.user) {
-            const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
 
-            return userData;
-          }
-          throw AuthenticationError('You are NOT logged in.');
+                return userData;
+            }
+            throw AuthenticationError('You are NOT logged in.');
         },
-      },
-      users: async () => {
-        return User.find().select('-__v -password');
-      },
-      user: async (parent, { username }) => {
-        return User.findOne({ username }).select('-__v -password');
-      },
-  
+        users: async () => {
+            return User.find().select('-__v -password');
+        },
+        user: async (parent, { username }) => {
+            return User.findOne({ username }).select('-__v -password');
+        },
+    },
 
-}
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw AuthenticationError;
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw AuthenticationError('Try email and/or password again.');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        saveBook: async (parent, { bookData }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { savedBooks: bookData } },
+                    { new: true }
+                );
+
+                return updatedUser;
+            }
+            throw new AuthenticationError('Log in please.');
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: {bookId} } },
+                    { new: true }
+                );
+
+                return updatedUser;
+            }
+            throw new AuthenticationError('Log in please.');
+        },
+    }
+};
 
 module.exports = resolvers;
