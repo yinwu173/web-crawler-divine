423f04fc0b01f6e03d92023c78404936baebcc4d
yinwu173 <yin.wu173@gmail.com>
2024-12-02 22:45:13 -0800

    imported required packages to App.jsx: constructred graphql api endpoint.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
 
 function App() {
   return (
